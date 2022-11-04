eb0d85b7a64140c474d39e85b243fefbbd364a6d
rakibul58 <rhrahi14@gmail.com>
2022-11-04 19:12:00 +0600

    some components added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import router from './Router/Routes/Routes';
 
 function App() {
   return (
-    <div>
-        <h1>Hello world</h1>
+    <div className='max-w-screen-xl mx-auto'>
+        <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
