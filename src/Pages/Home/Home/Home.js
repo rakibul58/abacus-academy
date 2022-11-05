52f31bf0ff3f8e3d45226818f622551a98400959
rakibul58 <rhrahi14@gmail.com>
2022-11-05 10:41:37 +0600

    light dark theme added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1 className='text-4xl font-bold text-error'>Hello</h1>
+            <h1 className='text-4xl font-bold text-primary'>Hello</h1>
         </div>
     );
 };
