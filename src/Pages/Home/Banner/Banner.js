8d9f804bf71b273967fc63a16d7cbdd311a215a4
rakibul58 <rhrahi14@gmail.com>
2022-11-06 19:35:14 +0600

    banner added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,28 +1,48 @@
 import React from 'react';
+import img1 from '../../../images/1.jpg';
+import img2 from '../../../images/2.jpg';
+import img3 from '../../../images/3.jpg';
+import img5 from '../../../images/5.jpg';
+import BannerItems from './BannerItems';
 
 const Banner = () => {
+    const bannerData = [
+        {
+            image: img1,
+            prev: 6,
+            id: 1,
+            next: 2
+        },
+        {
+            image: img2,
+            prev: 1,
+            id: 2,
+            next: 3
+        },
+        {
+            image: img3,
+            prev: 2,
+            id: 3,
+            next: 4
+        },
+        {
+            image: img5,
+            prev: 3,
+            id: 4,
+            next: 1
+        },
+        
+    ]
     return (
-        <div>
-            <div className="carousel w-full h-full">
-                <div id="item1" className="carousel-item w-full hero-overlay bg-opacity-95">
-                    <img alt='' src="https://placeimg.com/800/200/arch" className="w-full" />
-                </div>
-                <div id="item2" className="carousel-item w-full">
-                    <img alt='' src="https://placeimg.com/800/200/arch" className="w-full" />
-                </div>
-                <div id="item3" className="carousel-item w-full">
-                    <img alt='' src="https://placeimg.com/800/200/arch" className="w-full" />
-                </div>
-                <div id="item4" className="carousel-item w-full">
-                    <img alt='' src="https://placeimg.com/800/200/arch" className="w-full" />
-                </div>
-            </div>
-            <div className="flex justify-center w-full py-2 gap-2">
-                <a href="#item1" className="btn btn-xs">1</a>
-                <a href="#item2" className="btn btn-xs">2</a>
-                <a href="#item3" className="btn btn-xs">3</a>
-                <a href="#item4" className="btn btn-xs">4</a>
-            </div>
+        <div className="carousel w-full py-10">
+            {
+                bannerData.map(slide => <BannerItems
+                key={slide.id} 
+                slide={slide}
+                >
+
+                </BannerItems>)
+            }
         </div>
     );
 };
---
+++ src/Pages/Home/Banner/BannerItems.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './BannerItems.css';
+const BannerItems = ({slide}) => {
+    const {image , id , next , prev} = slide;
+    return (
+        <div id={`slide${id}`} className="carousel-item relative w-full">
+            <div className='carousel-img'>
+                <img src={image} alt='' style={{height: '800px' , width: '100%'}} className="w-full rounded-xl" />
+            </div>
+            <div className="absolute flex transform -translate-y-1/2 left-10 lg:left-24 top-1/3">
+                <h1 className='text-white font-semibold lg:font-bold text-3xl lg:text-5xl'>Affordable<br />
+                    Price for all<br />
+                    Course</h1>
+            </div>
+            <div className="absolute flex transform -translate-y-1/2 left-24 top-1/2">
+                <p className='text-white font-semibold text-lg hidden lg:flex w-96 mt-5'>There are many courses available, but the majority have enrolled to this as it is the best. Join us and we will help you to achieve your dream.</p>
+            </div>
+            <div className="absolute flex transform -translate-y-1/2 left-10 lg:left-24 top-1/2 lg:top-2/3 justify-center">
+                <Link><button className="btn btn-error hover:bg-transparent hover:text-white mr-2">Discover More</button></Link>
+                <Link><button className="btn btn-outline btn-error">Appointment</button></Link>
+            </div>
+            <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                <a href={`#slide${prev}`} className="btn btn-circle mr-5 bg-black bg-opacity-60">❮</a>
+                <a href={`#slide${next}`} className="btn btn-circle bg-error border-none">❯</a>
+            </div>
+        </div>
+    );
+};
+
+export default BannerItems;
