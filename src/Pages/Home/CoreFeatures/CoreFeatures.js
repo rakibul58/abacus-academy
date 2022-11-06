ad24b5ebdb951ea02474080d2b7eab0f3d43a935
Tonmoy-Roy <emonismail69@gmail.com>
2022-11-06 21:56:54 +0600

    info te kichu baki

--- src/Pages/Home/CoreFeatures/CoreFeatures.js
+++ src/Pages/Home/CoreFeatures/CoreFeatures.js
@@ -11,27 +11,28 @@ const CoreFeatures = () => {
             <p className='text-center m-5'>We are best for free online Courses.we believe in Quality.  <br /> Our Goal is spreading education around.</p>
            </div>
 
-           <div className='flex flex-wrap justify-center gap-10 my-10'>
+           <div className='flex flex-wrap justify-center gap-10 my-10 '>
            <div className='border-2 shadow-2xl p-4 hover:bg-orange-500'>
-           <div className='flex  justify-center'>
+           <div className='flex  justify-center '>
            <FcConferenceCall></FcConferenceCall>
+         
            </div >
-            <Link><h4>Expert Teams</h4></Link>
+            <Link><h4 className='font-medium text-2xl'>Expert Teams</h4></Link>
             </div>
 
            <div className='border-2 shadow-2xl p-4 hover:bg-orange-500'>
           <div className='flex  justify-center'><FcAssistant></FcAssistant></div>
-           <Link><h4>24/7 Support</h4></Link>
+           <Link><h4 className='font-medium text-2xl'>24/7 Support</h4></Link>
            </div>
 
            <div className='border-2 shadow-2xl p-4 hover:bg-orange-500'>
           <div className='flex  justify-center'>  <FcBusinessman></FcBusinessman></div>
-           <Link><h4>Skillfull Mentors</h4></Link>
+           <Link><h4 className='font-medium text-2xl'>Skillfull Mentors</h4></Link>
            </div>
 
            <div className='border-2 shadow-2xl p-4 hover:bg-orange-500'>
            <div className='flex  justify-center ' > <FcRating></FcRating></div>
-            <Link><h4>Believe in Quality</h4></Link>
+            <Link><h4 className='font-medium text-2xl'>Believe in Quality</h4></Link>
            </div>
 
            </div>
