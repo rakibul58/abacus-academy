d8c41e3e930674a042aa9083005ff19af474b86a
rakibul58 <rhrahi14@gmail.com>
2022-11-06 19:52:00 +0600

    folder added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import CoreFeatures from '../CoreFeatures/CoreFeatures';
+import Info from '../Info/Info';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <CoreFeatures></CoreFeatures>
+            <Info></Info>
         </div>
     );
 };
